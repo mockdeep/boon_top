bd634712a594ce9491af203d28f44c3bce3100b1
Robert Fletcher <lobatifricha@gmail.com>
2026-05-29 19:05:30 -0700

    clean up middleman deps

--- Gemfile
+++ Gemfile
@@ -2,8 +2,7 @@ source 'https://rubygems.org'
 
 ruby '4.0.5'
 
-gem 'middleman', '~> 4.6'
-gem 'middleman-livereload', '~> 3.4.0' # 3.5.0 requires Ruby >= 2.7
+gem 'middleman'
 
 gem 'rack-contrib'
 gem 'puma'
--- config.rb
+++ config.rb
@@ -35,10 +35,6 @@
 # Automatic image dimensions on image_tag helper
 # activate :automatic_image_sizes
 
-configure :development do
-  activate :livereload
-end
-
 # Methods defined in the helpers block are available in templates
 # helpers do
 #   def some_helper
